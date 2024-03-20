ffb6a1c70ba9bf0bd4aa3b5609a33eaf60b32c4d
Daniel Chung <danielchungfung@gmail.com>
2024-03-20 14:08:37 -0400

    fix: seo updates

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -67,6 +67,29 @@ definePageMeta({
 	layout: "landing",
 });
 
+useSeoMeta({
+	// HTML Meta Tags
+	title: "{{ doc.title }}",
+	description:
+		"Engage in every story and lesson we learn as we build Waffle.",
+	// Facebook Meta Tags
+	ogUrl: "https://blog.heywaffle.app/stories",
+	ogType: "website",
+	ogTitle: "Waffle Blog — Founder stories",
+	ogDescription:
+		"Engage in every story and lesson we learn as we build Waffle.",
+	ogImage: "https://alpha.n3-waffle.pages.dev/thumb2.webp",
+	ogImageAlt:
+		"Waffle is the visual organization app. Home to all your life's essentials.",
+	// Twitter Meta Tags
+	twitterCard: "summary_large_image",
+	twitterSite: "@heywaffleapp",
+	twitterTitle: "Waffle Blog — Founder stories",
+	twitterDescription:
+		"Engage in every story and lesson we learn as we build Waffle.",
+	twitterImage: "https://alpha.n3-waffle.pages.dev/thumb2.webp",
+});
+
 function readableDate(date) {
 	const res = new Date(date).toLocaleString("en-US", {
 		month: "long",
