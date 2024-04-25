3a9974c6e4cd50f29bbb7aefa356a398c1b64c78
Daniel Chung <danielchungfung@gmail.com>
2024-04-25 11:14:22 -0400

    update

--- components/Pages/Button.vue
+++ components/Pages/Button.vue
@@ -1,12 +1,16 @@
 <template>
-	<div class="flex flex-row items-center justify-center gap-[8px] absolute top-0 right-0">
+	<div
+		class="flex flex-row items-center justify-center gap-[8px] absolute top-0 right-0"
+	>
 		<NuxtLink
 			class="font-semibold text-[15px] text-center text-black hover:text-gray-850 leading-none select-none grid place-items-center px-[15px] h-[35px] border-black rounded-lg cursor-pointer"
 			to="https://heywaffle.app/signin"
-		>Sign in</NuxtLink>
-		<div
-			class="border-2 shadow font-semibold text-[15px] text-center text-white leading-none select-none grid place-items-center bg-black hover:bg-yellow-400 hover:text-black px-[15px] h-[35px] border-black rounded-lg cursor-pointer">
-			Join waitlist
-		</div>
+			>Sign in</NuxtLink
+		>
+		<NuxtLink
+			class="shadow font-semibold text-[15px] text-center text-white leading-none grid place-items-center bg-black hover:bg-yellow-400 hover:text-black outline-offset-4 px-[15px] h-[40px] rounded-lg cursor-pointer"
+			to="https://heywaffle.app/"
+			>Join waitlist</NuxtLink
+		>
 	</div>
 </template>
