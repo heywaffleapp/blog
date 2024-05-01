986968b7e2b7650b3e5a7bd89d01ff62d4435ce3
Daniel Chung <danielchungfung@gmail.com>
2024-05-01 10:42:34 -0400

    update logo

--- components/Pages/Logo.vue
+++ components/Pages/Logo.vue
@@ -1,5 +1,8 @@
 <template>
-	<NuxtLink class="absolute top-0 left-0 h-[25px] cursor-pointer" to="/">
+	<NuxtLink
+		class="flex flex-row gap-2 items-center absolute top-0 left-0 h-[25px] cursor-pointer"
+		to="/"
+	>
 		<img src="/logo.png" class="h-[30px]" alt="Waffle's logo" />
 		<div
 			class="bg-gray-100 text-[12px] rounded-[10px] px-[6px] py-[4px] font-semibold text-gray-800 leading-none"
